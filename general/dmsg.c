5b81281f1347507d1e0258a21934015a393df380
KongQBin <51024054@QQ.com>
2023-09-14 10:29:27 +0800

    feat(dmsg):对print进一步封装，方便后续统一管理打印信息

---
+++ general/dmsg.c
@@ -0,0 +1,10 @@
+#include "general.h"
+#include <stdarg.h>
+//inline void dmsg(const char *fmt, __gnuc_va_list __arg)
+inline void dmsg(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap,fmt);
+    vprintf(fmt,ap);
+    va_end(ap);
+}
