ef2f4f7efff0c512a68a5ca31e65d205c0ea7357
KongQBin <51024054@QQ.com>
2023-10-18 13:17:01 +0800

    style(msg):调整色彩输出

--- general/general.h
+++ general/general.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdio.h>
 #include <stdarg.h>
+#include "cbdefine.h"
+
 // MsgLevel与color的元素个数必须对应，否则将会触发段错误
 enum MsgLevel
 {
@@ -13,10 +15,15 @@ enum MsgLevel
 };
 
 static char color[][32] = {
-    "\033[1;31;5m",            // 亮红
+    "\033[1;31m",            // 亮红
+//    "\033[1;31;5m",            // 亮红闪
     //    "\033[0;32;31m",         // 暗红
-    "\033[0;33;5m",            // 棕色
-    "\033[1;33;5m",            // 亮黄
+
+    "\033[0;33m",            // 棕色
+    "\033[1;33m",            // 亮黄
+//    "\033[0;33;5m",            // 棕色闪
+//    "\033[1;33;5m",            // 亮黄闪
+
     "\033[1;37m",            // 白
     "\033[1;36m",            // 青蓝
     //    "\033[1;34m",            // 亮蓝
@@ -25,12 +32,10 @@ static char color[][32] = {
     //    "\033[0;32;32m",         // 暗绿
 };
 
-static inline void dmsg(const char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap,fmt);
-    vprintf(fmt,ap);
-    va_end(ap);
+#define dmsg(fmt, ...)\
+{\
+    printf("%llu ",gettid());\
+    printf(fmt,##__VA_ARGS__);\
 }
 
 #define SPIK_LEVEN(level) if(/*level == ML_INFO*/0) break;
@@ -39,6 +44,7 @@ static inline void dmsg(const char *fmt, ...)
 do{\
     SPIK_LEVEN(level);\
     printf(color[level]);\
+    printf("%llu ",gettid());\
     printf("[%s:%d]:\t",__FILE__,__LINE__);\
     printf(fmt,##__VA_ARGS__);\
     printf(NCOLOR);\
